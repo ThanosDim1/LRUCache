401c37e11695cc0661a2ec781d20c5bfa6b9646e
ThanosDim1 <thanasis1105@gmail.com>
2024-02-26 22:20:17 +0200

    Main Project Files

---
+++ src/CacheImpl.java
@@ -0,0 +1,32 @@
+public class CacheImpl<K,V> implements Cache<K,V>{
+
+    @Override
+    public V lookUp(K key) {
+        return null;
+    }
+
+    @Override
+    public void store(K key, V value) {
+
+    }
+
+    @Override
+    public double getHitRatio() {
+        return 0;
+    }
+
+    @Override
+    public long getHits() {
+        return 0;
+    }
+
+    @Override
+    public long getMisses() {
+        return 0;
+    }
+
+    @Override
+    public long getNumberOfLookUps() {
+        return 0;
+    }
+}
